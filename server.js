fb542b793c10fca9346177509eed3d250cecd81c
SHOBHIT TIWARI <46121289+shobhu98@users.noreply.github.com>
2020-05-01 01:17:31 +0530

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const  express=require('express');
 const app=express();
 const PORT=process.env.PORT||5000;
+const  path=require('path');
 const connectDB=require('./config/db');
 
 connectDB();
@@ -12,6 +13,18 @@ app.use('/api/users',require('./routes/api/users'));
 app.use('/api/auth',require('./routes/api/auth'));
 app.use('/api/profile',require('./routes/api/profile'));
 app.use('/api/posts',require('./routes/api/posts'));
+
+
+//Serve static assets in production
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static('client/build'));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+
+}
+
+
 app.listen(PORT,function () {
 
     console.log("running on port "+PORT);
